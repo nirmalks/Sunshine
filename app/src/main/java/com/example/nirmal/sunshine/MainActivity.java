f0ae29f190b24ddf53703128ebc9376727cb5a67
nirmalks <kagosekar@gmail.com>
2016-02-17 17:35:09 +0530

    Have added some dummy data for the fragment in onCreateView method

--- app/src/main/java/com/example/nirmal/sunshine/MainActivity.java
+++ app/src/main/java/com/example/nirmal/sunshine/MainActivity.java
@@ -5,9 +5,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,5 +26,17 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
-    
+    public View onCreateView(LayoutInflater inflater , ViewGroup container, Bundle savedInstanceState){
+        View rootView = inflater.inflate(R.layout.fragment_main,container,false);
+
+        String forecastArray[] = {
+            "Today",
+                "Tomorrow",
+                "Sunday",
+                "Saturday"
+        };
+
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+        return rootView;
+    }
 }
