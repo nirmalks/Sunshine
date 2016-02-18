f7bbd6c477fff7cff7e6ebd9c5e42178d3ad3c12
nirmalks <kagosekar@gmail.com>
2016-02-18 23:07:33 +0530

    Removed onCreateView method and unused imports

--- app/src/main/java/com/example/nirmal/sunshine/MainActivity.java
+++ app/src/main/java/com/example/nirmal/sunshine/MainActivity.java
@@ -1,19 +1,9 @@
 package com.example.nirmal.sunshine;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.ViewGroup;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,17 +16,5 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
     }
 
-    public View onCreateView(LayoutInflater inflater , ViewGroup container, Bundle savedInstanceState){
-        View rootView = inflater.inflate(R.layout.fragment_main,container,false);
-
-        String forecastArray[] = {
-            "Today",
-                "Tomorrow",
-                "Sunday",
-                "Saturday"
-        };
-
-        List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
-        return rootView;
-    }
 }
+
