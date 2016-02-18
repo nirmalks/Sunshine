2d43beca04ef6a25d3bf9ea62222399d51894cd0
nirmalks <kagosekar@gmail.com>
2016-02-18 23:06:26 +0530

    Swapped the fragment code(onCreateView) from MainActivity.java to MainActivityFragment.java

--- app/src/main/java/com/example/nirmal/sunshine/MainActivityFragment.java
+++ app/src/main/java/com/example/nirmal/sunshine/MainActivityFragment.java
@@ -1,15 +1,23 @@
 package com.example.nirmal.sunshine;
 
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    private ArrayAdapter<String> mForecastAdapter;
 
     public MainActivityFragment() {
     }
@@ -17,6 +25,36 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);;
+
+        //Dummy data
+        String[] forecastArray = {
+                "Monday - Sunny",
+                "Tuesday - Cloudy",
+                "Thursday - Chilly",
+                "Friday- I dont know",
+                "Saturday - Unpredicatble",
+                "Sunday - Stormy "
+        };
+
+
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+        mForecastAdapter = new ArrayAdapter<String>(
+                //The current context - Fragment's parent activity
+                getActivity(),
+                //ID of list item layout
+                R.layout.list_item_forecast,
+                //ID of the textView to populate
+                R.id.list_item_forecast_textview,
+                //data
+                weekForecast);
+
+
+        // Reference to the ListView, and attach this adapter to it.
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+        listView.setAdapter(mForecastAdapter);
+
+        return rootView;
     }
 }
